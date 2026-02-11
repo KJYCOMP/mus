33a1e30114d534bb1bc1d95989704f54662a63e4
KJYCOMP <125692122+KJYCOMP@users.noreply.github.com>
2026-02-11 14:46:25 +0800

    74

--- javbus.js
+++ javbus.js
@@ -1,97 +1,32 @@
 /**
- * JavBus 本地 Spider - 兼容版
+ * JavBus 本地 Spider - 三级架构版 (GitHub)
  */
+var workerUrl = "https://hak.486253sg.eu.org"; // 你的 HAK 网关地址
 
-var workerUrl = "https://hak.486253sg.eu.org"; // 你的域名
-var siteUrl = "https://kt.guykjy.useruno.com";
-
-function init(ext) {
-    console.log("JavBus Spider Initialized");
-}
+function init(ext) { console.log("JavBus Init"); }
 
 function home(filter) {
-    var classes = [{"type_id": "anc", "type_name": "✨ 南瓜架构·本地爬虫"}];
-    var filters = {
-        "anc": [{
-            "key": "f",
-            "name": "类型标签",
-            "value": [
-                {"n": "👣 丝足", "v": "28"},
-                {"n": "⛓️ 折磨", "v": "62"},
-                {"n": "🤮 呕吐", "v": "5g"},
-                {"n": "🐙 触手", "v": "59"},
-                {"n": "👶 处男", "v": "52"}
-            ]
-        }]
-    };
+    var classes = [{"type_id": "anc", "type_name": "✨ 并发 12 页·海报墙"}];
+    var filters = {"anc": [{"key": "f", "name": "分类", "value": [{"n": "👣 丝足", "v": "28"},{"n": "⛓️ 折磨", "v": "62"},{"n": "🤮 呕吐", "v": "5g"},{"n": "🐙 触手", "v": "59"}]}]};
     return JSON.stringify({ "class": classes, "filters": filters });
 }
 
 function category(tid, pg, filter, extend) {
     var fValue = (extend && extend.f) ? extend.f : "28";
-    var page = pg || 1;
-    var url = siteUrl + "/api/movies?filterType=genre&filterValue=" + fValue + "&page=" + page + "&magnet=all";
-    
-    var response = http.get(url, { headers: { "User-Agent": "Mozilla/5.0" } });
-    var json = JSON.parse(response);
-    var videos = [];
-    
-    if (json && json.movies) {
-        json.movies.forEach(function(it) {
-            videos.push({
-                "vod_id": it.id,
-                "vod_name": "[" + it.id + "] " + it.title,
-                // 这里不要再编码，直接交给 Worker 处理
-                "vod_pic": workerUrl + "/proxy-img/" + it.img,
-                "vod_remarks": it.date || ""
-            });
-        });
-    }
-
-    return JSON.stringify({
-        "page": page,
-        "pagecount": 1, 
-        "limit": videos.length,
-        "total": 999,
-        "list": videos
-    });
+    // 脚本向 HAK 请求，HAK 会去找 anc
+    var url = workerUrl + "/list?filterValue=" + fValue + "&pg=" + pg;
+    var res = http.get(url, { headers: { "User-Agent": "Mozilla/5.0" } });
+    return res; 
 }
 
 function detail(id) {
-    var url = siteUrl + "/api/movies/" + id;
-    var response = http.get(url, { headers: { "User-Agent": "Mozilla/5.0" } });
-    var it = JSON.parse(response);
-    
-    var vod = {
-        "vod_id": it.id,
-        "vod_name": it.title,
-        "vod_pic": workerUrl + "/proxy-img/" + it.img,
-        "type_name": "JavBus",
-        "vod_content": it.description || it.title,
-        "vod_play_from": "高清磁力链接",
-        "vod_play_url": "立即播放$" + workerUrl + "/play?id=" + it.id
-    };
-    
-    return JSON.stringify({ "list": [vod] });
+    var url = workerUrl + "/detail?ids=" + id;
+    return http.get(url, { headers: { "User-Agent": "Mozilla/5.0" } });
 }
 
-function search(wd, quick) {
-    var url = siteUrl + "/api/movies/search?keyword=" + encodeURIComponent(wd) + "&page=1&magnet=all";
-    var response = http.get(url, { headers: { "User-Agent": "Mozilla/5.0" } });
-    var json = JSON.parse(response);
-    var videos = [];
-    if (json && json.movies) {
-        json.movies.forEach(function(it) {
-            videos.push({
-                "vod_id": it.id,
-                "vod_name": it.title,
-                "vod_pic": workerUrl + "/proxy-img/" + it.img,
-                "vod_remarks": it.date
-            });
-        });
-    }
-    return JSON.stringify({ "list": videos });
+function search(wd) {
+    var url = workerUrl + "/search?wd=" + encodeURIComponent(wd);
+    return http.get(url, { headers: { "User-Agent": "Mozilla/5.0" } });
 }
 
-// 导出模块 (保持最高兼容性)
 export default { init, home, category, detail, search };
