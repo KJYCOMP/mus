64d236753267f2da02835f74d9f4103d7c21c1b9
KJYCOMP <125692122+KJYCOMP@users.noreply.github.com>
2026-02-11 14:50:01 +0800

    78

--- javbus.js
+++ javbus.js
@@ -1,30 +1,31 @@
 /**
- * JavBus 本地 Spider - 三级架构版 (GitHub)
+ * JavBus 本地 Spider - 终极稳定版 (GitHub)
  */
-var workerUrl = "https://hak.486253sg.eu.org"; // 你的 HAK 网关地址
+var workerUrl = "https://hak.486253sg.eu.org"; 
 
 function init(ext) { console.log("JavBus Init"); }
 
 function home(filter) {
-    var classes = [{"type_id": "anc", "type_name": "✨ 并发 12 页·海报墙"}];
+    var classes = [{"type_id": "anc", "type_name": "✨ 12页并发·稳定版"}];
     var filters = {"anc": [{"key": "f", "name": "分类", "value": [{"n": "👣 丝足", "v": "28"},{"n": "⛓️ 折磨", "v": "62"},{"n": "🤮 呕吐", "v": "5g"},{"n": "🐙 触手", "v": "59"}]}]};
     return JSON.stringify({ "class": classes, "filters": filters });
 }
 
 function category(tid, pg, filter, extend) {
     var fValue = (extend && extend.f) ? extend.f : "28";
-    // 脚本向 HAK 请求，HAK 会去找 anc
+    // 显式指向 /list 路径
     var url = workerUrl + "/list?filterValue=" + fValue + "&pg=" + pg;
-    var res = http.get(url, { headers: { "User-Agent": "Mozilla/5.0" } });
-    return res; 
+    return http.get(url, { headers: { "User-Agent": "Mozilla/5.0" } });
 }
 
 function detail(id) {
+    // 显式指向 /detail 路径
     var url = workerUrl + "/detail?ids=" + id;
     return http.get(url, { headers: { "User-Agent": "Mozilla/5.0" } });
 }
 
 function search(wd) {
+    // 显式指向 /search 路径
     var url = workerUrl + "/search?wd=" + encodeURIComponent(wd);
     return http.get(url, { headers: { "User-Agent": "Mozilla/5.0" } });
 }
